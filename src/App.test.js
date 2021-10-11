a5c2a68a00975e2a9a6140f236d40e79cc82828d
Pavan Rathod <edroculllen13@gmail.com>
2021-10-11 12:13:02 +0530

    jsut commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
 
 test("renders learn react app", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  expect(div.innerHTML).toContain("react testing");
+  ReactDOM.unmountComponentAtNode(div);
 });
