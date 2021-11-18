a5f072e27f294c8080661017d185593d3d6e77b4
Pavan Rathod <edroculllen13@gmail.com>
2021-11-18 22:27:38 +0530

    this is formik

--- src/App.js
+++ src/App.js
@@ -1,34 +1,118 @@
+import { Formik, Field, Form, useField } from "formik";
 import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link,
-  useParams,
-  useRouteMatch,
-} from "react-router-dom";
+  Button,
+  TextField,
+  Checkbox,
+  Radio,
+  FormControlLabel,
+} from "@material-ui/core";
 
-import React from "react";
+import * as yup from "yup";
 
-import Homepage from "./pages/Homepage";
-import Publicpage from "./pages/Publicpage";
-import Privatepage from "./pages/Privatepage";
-import Header from "./components/Header";
-import PrivateRoutes from "./Routes/PrivateRoutes";
-import Loginpage from "./pages/Loginpage";
+const MyRadio = ({ label, ...props }) => {
+  const [field] = useField(props);
+  return <FormControlLabel {...field} control={<Radio />} label={label} />;
+};
+
+const MyTextField = ({ placeholder, ...props }) => {
+  const [field, meta] = useField(props);
+  const errorText = meta.error && meta.touched ? meta.error : "";
+  return (
+    <TextField
+      placeholder={placeholder}
+      {...field}
+      helperText={errorText}
+      error={!!errorText}
+    />
+  );
+};
+
+const validateSchema = yup.object({
+  firstName: yup.string().required().max(10),
+});
 
 const App = () => {
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    isTall: false,
+    cookies: [],
+    yogurt: "",
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/public" component={Publicpage} />
-        <Route exact path="/" component={Homepage} />
-        <Route path="/login" component={Loginpage} />
-        <PrivateRoutes path="/private">
-          <Privatepage />
-        </PrivateRoutes>
-      </Switch>
-    </BrowserRouter>
+    <div>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(data) => onSubmit(data)}
+        validationSchema={validateSchema}
+
+        // validate={(values) => {
+        //   const errors = {};
+
+        //   if (values.firstName.includes("bob")) {
+        //     errors.firstName = "No bob";
+        //   }
+        //   return errors;
+        // }}
+      >
+        {({ values, errors }) => (
+          <Form>
+            <MyTextField
+              placeholder="firstName"
+              name="firstName"
+              type="input"
+              as={TextField}
+            />
+            <div>
+              <Field
+                placeholder="lastName"
+                name="lastName"
+                type="input"
+                as={TextField}
+              />
+            </div>
+            <Field type="checkbox" as={Checkbox} name="isTall" />
+            <div>Cookies:</div>
+            <Field
+              type="checkbox"
+              as={Checkbox}
+              value="suger bites"
+              name="cookies"
+            />
+            <Field
+              type="checkbox"
+              as={Checkbox}
+              value="Chacko punch"
+              name="cookies"
+            />
+            <Field
+              type="checkbox"
+              as={Checkbox}
+              value="Parle g"
+              name="cookies"
+            />
+            <div>Yogurt:</div>
+            <MyRadio type="radio" name="yogurt" value="peach" label="peach" />
+            <MyRadio
+              type="radio"
+              name="yogurt"
+              value="blueberry"
+              label="blueberry"
+            />
+            <MyRadio type="radio" name="yogurt" value="candy" label="candy" />
+            <div>
+              <Button type="submit">submit</Button>
+            </div>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+            <pre>{JSON.stringify(errors, null, 2)}</pre>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
